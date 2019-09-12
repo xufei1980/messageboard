6389c6f935c975456216f0f20aa9db5abe7740a7
xufei1980 <xf2100@gmail.com>
2019-09-12 13:35:18 +1200

    messagebox

---
+++ src/components/messagebox.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class Messagebox extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <div className="row">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        <div className="card">
+                            <div className="card-header"></div>
+                            <div className="card-body"></div>
+                            <div className="card-footer"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Messagebox
